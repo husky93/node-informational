124f0f9b66421d262dd0ebcebb25b80237ec3e4e
Maciej Sroka <sroka.maciej93@gmail.com>
2022-11-06 15:24:17 +0100

    Serve correct html file for Homepage

--- index.js
+++ index.js
@@ -4,7 +4,18 @@ const path = require('path');
 
 const PORT = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {});
+const server = http.createServer((req, res) => {
+  if (req.url === '/') {
+    fs.readFile(
+      path.join(__dirname, 'public', 'index.html'),
+      (error, content) => {
+        if (error) throw error;
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(content);
+      }
+    );
+  }
+});
 
 server.listen(PORT, () => {
   console.log(`Server running at port ${PORT}`);
